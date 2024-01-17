922e1f3d2f43303b8250b3fda08af3fd26601240
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-17 21:31:08 +0000

    files  and folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+ 
+
+  return (
+    <>
+    <div className="text-6xl text-center font-mono text-blue-600 bg-black p-5 rounded-md">
+      Testing tailwind
+    </div>
+    </>
+  )
+}
+
+export default App
