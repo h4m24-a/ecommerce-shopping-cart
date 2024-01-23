53f7b20fbcd74d66420ae910413dc9241d2de00a
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-23 17:48:10 +0000

    fix spacing issues

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,4 +1,3 @@
-
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 import Card from "../components/ui/Card";
 
@@ -10,15 +9,32 @@ const BestSeller = () => {
           Best Seller
         </h1>
         <div className="flex flex-row items-center gap-3">
-            <p className="text-xl font-sora text-center cursor-pointer ">See All</p>
-            <ArrowUpRightIcon className="h-9 w-9" />
+          <p className="text-xl font-sora text-center cursor-pointer ">
+            See All
+          </p>
+          <ArrowUpRightIcon className="h-9 w-9" />
         </div>
       </div>
-        <div className="flex mt-6 items-center justify-center gap-8  flex-col lg:flex-row lg">
-        <Card  imageSrc="../assets/bestseller-1.jpg" imageStyle="object-cover w-full h-full rounded-sm cursor-pointer" textStyle=" p-2 " description="Trench Coat"/>
-        <Card  imageSrc="../assets/bestseller-2.jpg" imageStyle="object-cover w-full h-full rounded-sm cursor-pointer" textStyle=" p-2 " description="All Black"/>
-        <Card  imageSrc="../assets/bestseller-3.jpg" imageStyle="object-cover w-full h-full rounded-sm cursor-pointer" textStyle=" p-2 " description="Grey Jacket"/>
-        </div>
+      <div className="flex mt-6 items-center justify-center gap-8  flex-col lg:flex-row lg">
+        <Card
+          imageSrc="../assets/bestseller-1.jpg"
+          imageStyle="object-cover w-full h-full rounded-sm cursor-pointer"
+          textStyle=" py-2 "
+          description="Trench Coat"
+        />
+        <Card
+          imageSrc="../assets/bestseller-2.jpg"
+          imageStyle="object-cover w-full h-full rounded-sm cursor-pointer"
+          textStyle=" py-2 "
+          description="All Black"
+        />
+        <Card
+          imageSrc="../assets/bestseller-3.jpg"
+          imageStyle="object-cover w-full h-full rounded-sm cursor-pointer"
+          textStyle=" py-2 "
+          description="Grey Jacket"
+        />
+      </div>
     </div>
   );
 };
