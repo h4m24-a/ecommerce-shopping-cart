9dea82e902b0dded9d95d38a55d2600491e9e9e5
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-25 21:56:14 +0000

    styled cart page

--- src/components/ui/Quantity.jsx
+++ src/components/ui/Quantity.jsx
@@ -4,15 +4,15 @@ import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 const Quantity = ({ value, onDecrease, onIncrease }) => {
   return (
     <div>
-      <div className="border border-gray-400 mx-auto w-fit p-2">
+      <div className="border flex flex-row justify-center border-gray-400 bg-gray-200 rounded-lg mx-auto w-36 p-2">
         <label htmlFor="Quantity" className="sr-only">
           Quantity
         </label>
 
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <button
             type="button"
-            className="h-5 w-5 leading-5 text-black text-lg transition hover:opacity-75"
+            className="h-3 w-3 leading-3 text-black text-md transition hover:opacity-75"
             onClick={onDecrease}
           >
             <MinusIcon />
@@ -22,13 +22,13 @@ const Quantity = ({ value, onDecrease, onIncrease }) => {
             type="number"
             id="Quantity"
             value={value}
-            className="h-5 w-16 rounded text-center text-md lg:text-xl"
+            className="h-3 font-montserrat bg-gray-200  w-16 rounded text-center text-sm lg:text-md"
             readOnly
           />
 
           <button
             type="button"
-            className="h-5 w-5 leading-8 text-black text-lg transition hover:opacity-75"
+            className="h-3 w-3 leading-8 text-black text-md transition hover:opacity-75"
             onClick={onIncrease}
           >
             <PlusIcon />
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,9 +3,10 @@ import Footer from "../components/Footer";
 import Quantity from "../components/ui/Quantity";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import Button from "../components/ui/Button";
 
 const Cart = ({ carts }) => {
-  
   const [quantity, setQuantity] = useState(1);
 
   const updateQuantity = (productId, newQuantity) => {
@@ -15,11 +16,13 @@ const Cart = ({ carts }) => {
     }));
   };
 
-  const decreaseQuantity = (productId) => {
-    updateQuantity(productId, Math.max(quantity[productId] - 1, 1));   // first -1 is to decrease by one, second 1 is make sure that its never less than one
-  };
+  const handleCheckout = () => {
+    alert("Thank you for your order!")
+  }
 
- 
+  const decreaseQuantity = (productId) => {
+    updateQuantity(productId, Math.max(quantity[productId] - 1, 1)); // first -1 is to decrease by one, second 1 is make sure that its never less than one
+  };
 
   const increaseQuantity = (productId) => {
     updateQuantity(productId, (quantity[productId] || 0) + 1);
@@ -29,26 +32,47 @@ const Cart = ({ carts }) => {
     <>
       <NavBar />
 
-      <div className="flex flex-col">
-        {carts.map((product) => (
-          <div key={product.id}>
-            <img
-              className="h-32 w-32 rounded-md aspect-square object-cover"
-              src={product.image}
-              alt={product.name}
-            />
-            <p>{product.name}</p>
-
-            <div className="flex flex-col">
-              <Quantity
-                value={quantity[product.id] || 0}
-                onDecrease={() => decreaseQuantity(product.id)}
-                onIncrease={() => increaseQuantity(product.id)}
+      <h1 className="text-3xl text-center font-poppins font-bold mb-4">
+        Your Shopping Cart
+      </h1>
+      <div className="container w-full mt-5  mx-auto flex-col flex justify-evenly p-4 lg:flex-row ">
+        <div className="flex flex-col gap-3 items-center">
+          {carts.map((product) => (
+            <div
+              key={product.id}
+              className=" border-2 border-gray-300 rounded-lg  flex flex-row  w-full items-center gap-5  p-4 md:justify-center lg:gap-20 "
+            >
+              <img
+                className="h-32 w-32 rounded-md object-cover mb-4"
+                src={product.image}
+                alt={product.name}
               />
-              <p>{product.price}</p>
+
+              <div className="flex flex-col gap-3">
+                <p className="text-sm font-sora font-medium">{product.name}</p>
+                <p className="text-sm font-sora font-medium">
+                  ${product.price}
+                </p>
+                <Quantity
+                  value={quantity[product.id] || 0}
+                  onDecrease={() => decreaseQuantity(product.id)}
+                  onIncrease={() => increaseQuantity(product.id)}
+                />
+              </div>
+              <TrashIcon className="w-14 h-14 hover:bg-gray-300 p-3 active:text-red-500 cursor-pointer" />
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        
+        <div className="flex flex-col gap-3">
+        <h2 className="font-poppins text-lg text">Your order summary</h2>
+        <p className=" text-md font-montserrat">Total $xx</p>
+        <Button 
+        btnClass="text-white w-full font-poppins bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        text="Checkout"
+        onClick={handleCheckout} />
+
+        </div>
       </div>
 
       <Footer />
