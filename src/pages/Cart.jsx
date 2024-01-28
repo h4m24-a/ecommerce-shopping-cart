30f693bc27a398dae233c0d90767601423af725d
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-28 22:22:04 +0000

    updated cart ui

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -54,58 +54,67 @@ const Cart = ({ carts, handleRemove }) => {
       <h1 className="text-3xl text-center font-poppins font-bold mb-4">
         Your Cart
       </h1>
-      <div className="container mb-56   flex-grow max-w-4xl mt-5 mx-auto flex-col flex justify-evenly p-4 lg:flex-row lg:gap-32">
-        <div className="flex flex-col  gap-3 items-center flex-1">
+      <div className="container mb-56 max-w-7xl mt-5 mx-auto flex-col flex  p-4 lg:flex-row lg:gap-20">
+        <div className="flex flex-col gap-3  items-center flex-1">
           {carts.map((product) => (
             <div
               key={product.id}
-              className="flex flex-row w-full items-center justify-center p-4 border-t border-b border-gray-300 md:justify-center lg:gap-20"
+              className="flex flex-col w-full  p-4 border-t border-b border-gray-300 lg:flex-row  lg:gap-20"
             >
               <img
-                className="h-32 w-32 rounded-md object-cover mb-4 flex-shrink-0"
+                className="h-32 w-32 rounded-md object-cover mb-4 "
                 src={product.image}
                 alt={product.name}
               />
 
-              <div className="flex flex-col h-full items-end gap-2 flex-1">
-                <XMarkIcon
-                  className="w-12 h-12 hover:outline-2 p-3 active:text-red-500 cursor-pointer"
-                  onClick={() => handleRemove(product.id)}
-                />
-                <p className="text-xs flex-wrap ml-2 font-sora lg:text-md font-medium">
-                  {product.name}
-                </p>
-                <p className=" text-xs font-sora font-medium lg:text-md">
-                  ${product.price}
-                </p>
-                <p className=" text-xs font-sora font-medium lg:text-md">
-                {product.size}
-                </p>
-                <Quantity
-                  value={quantity[product.id] || 1}
-                  onDecrease={() => decreaseQuantity(product.id)}
-                  onIncrease={() => increaseQuantity(product.id)}
-                />
+              <div className="flex flex-col h-full justify-center items-center gap-2 flex-1 ml-5 lg:flex-row lg:ml-0">
+                <div className="flex items-center justify-between w-full">
+                  <div className="flex flex-col gap-3 ">
+                    <p className="text-xs font-sora lg:text-md font-medium">
+                      {product.name}
+                    </p>
+                    <p className="text-xs font-sora font-medium lg:text-md">
+                      ${product.price}
+                    </p>
+                    <p className="text-xs font-sora font-medium lg:text-md">
+                      {product.size}
+                    </p>
+                  </div>
+                  <div  className="flex flex-col  items-center justify-center lg:gap-2">
+                    <XMarkIcon
+                      className="w-12 h-12 hover:outline-2 p-3 active:text-red-500 cursor-pointer"
+                      onClick={() => handleRemove(product.id)}
+                    />
+
+                  <Quantity
+                    value={quantity[product.id] || 1}
+                    onDecrease={() => decreaseQuantity(product.id)}
+                    onIncrease={() => increaseQuantity(product.id)}
+                  />
+                  </div>
+                </div>
               </div>
             </div>
           ))}
         </div>
 
         {carts.length > 0 && (
-         <div className="flex flex-col w-full h-fit mx-auto mt-6 pt-5 border just border-gray-200 items-center gap-5 lg:mt-0 lg:w-80">
-         <h2 className="font-montserrat w-full text-md uppercase border-b pb-3 text-center">Order summary</h2>
-         <p className=" text-md font-montserrat uppercase">Total <span className="font-bold ml-2 tracking-widest">${totalPrice}</span></p>
-         <Button
-           btnClass="text-white w-full uppercase font-poppins bg-gray-950 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5"
-           text="Checkout"
-           onClick={handleCheckout}
-         />
-
-         
-       </div>
+          <div className="flex flex-col w-full h-fit mx-auto mt-6 pt-5 border justify-center items-center gap-5 lg:mt-0 lg:w-80">
+            <h2 className="font-montserrat w-full text-md uppercase border-b pb-3 text-center">
+              Order summary
+            </h2>
+            <p className="text-md font-montserrat uppercase">
+              Total{" "}
+              <span className="font-bold ml-2 tracking-widest">${totalPrice}</span>
+            </p>
+            <Button
+              btnClass="text-white w-full uppercase font-poppins bg-gray-950 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5"
+              text="Checkout"
+              onClick={handleCheckout}
+            />
+          </div>
         )}
       </div>
-        
         
       <Footer />
     </>
