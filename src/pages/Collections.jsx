4e0b0c9b48be6fc4e972a54814bb36354bf8e515
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-29 19:10:11 +0000

    updated collections page

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,36 +1,48 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import PropTypes from "prop-types";
+import { ViewfinderCircleIcon } from "@heroicons/react/24/outline";
 
 const Collections = ({ carts }) => {
   return (
     <div>
       <NavBar badge={carts.length} />
       <section>
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-          <header className="text-center">
-            <h2 className="text-4xl font-montserrat font-bold text-gray-900">
-              2024 Winter Collection
+        <div className="mx-auto  px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <header className="text-center items-center flex flex-col justify-between lg:flex-row">
+            <h2 className="text-6xl font-montserrat  text-gray-900">
+              Enchant Streetwear
             </h2>
 
-            <p className="mx-auto mt-4 font-montserrat max-w-md text-gray-500">
-              Our designers have meticulously curated each piece, drawing
-              inspiration from timeless classics while infusing a modern edge.
-            </p>
+            <div className="flex flex-col mt-4 items-center justify-center lg:mt-0">
+              <p className=" font-montserrat text-3xl uppercase">
+                23/24
+                <span className="flex flex-row mt-3 items-center gap-2">
+                  <ViewfinderCircleIcon className="h-10 w-10" />
+                  Collection
+                </span>
+              </p>
+            </div>
           </header>
 
-          <ul className="mt-8 grid gap-1 sm:grid-cols-2 lg:grid-cols-4">
+          <img
+            className="h-full cursor-pointer max-w-full transition duration-500 hover:opacity-90 mt-10"
+            src="../public/assets/collections-1.jpg"
+            alt="fashion show"
+          />
+
+          <ul className="mt-16 grid gap-1 max-w-screen-xl mx-auto sm:grid-cols-2 lg:grid-cols-4">
             <li>
               <p href="#" className="group block overflow-hidden">
                 <img
                   src="https://img.freepik.com/premium-photo/man-stands-street-wearing-green-jacket-black-cap_943281-23344.jpg?size=626&ext=jpg&ga=GA1.2.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                   Jacket
+                    Jacket
                   </h3>
 
                   <p className="mt-2">
@@ -50,7 +62,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/angel-ai-generated_23-2150690347.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -75,7 +87,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/cyberpunk-warrior-urban-scenery_23-2150712356.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -100,7 +112,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/premium-photo/fashion-model-wearing-jacket_519183-9880.jpg?size=626&ext=jpg&ga=GA1.2.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -125,12 +137,12 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/cyberpunk-man-warrior-with-dog_23-2150712452.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                   Jacket
+                    Jacket
                   </h3>
 
                   <p className="mt-2">
@@ -150,12 +162,12 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/close-up-man-portrait-new-york-city_23-2150868124.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                   Jacket
+                    Jacket
                   </h3>
 
                   <p className="mt-2">
@@ -175,12 +187,12 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/close-up-man-new-york-streets_23-2150868150.jpg?size=626&ext=jpg&ga=GA1.2.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                   Jacket
+                    Jacket
                   </h3>
 
                   <p className="mt-2">
@@ -200,12 +212,12 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/portrait-person-during-daily-life-new-york-city_23-2150820002.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                   Jacket
+                    Jacket
                   </h3>
 
                   <p className="mt-2">
