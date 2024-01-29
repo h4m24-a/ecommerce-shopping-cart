174b1886a15c3bb3f963f2bacfaee0af2ade955a
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-29 19:41:08 +0000

    updated collections page

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -10,9 +10,9 @@ const Collections = ({ carts }) => {
       <section>
         <div className="mx-auto  px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
           <header className="text-center items-center flex flex-col justify-between lg:flex-row">
-            <h2 className="text-6xl font-montserrat  text-gray-900">
+            <h1 className="text-6xl font-montserrat  text-gray-900">
               Enchant Streetwear
-            </h2>
+            </h1>
 
             <div className="flex flex-col mt-4 items-center justify-center lg:mt-0">
               <p className=" font-montserrat text-3xl uppercase">
@@ -30,14 +30,18 @@ const Collections = ({ carts }) => {
             src="../public/assets/collections-1.jpg"
             alt="fashion show"
           />
+          <h2 className=" font-montserrat mt-10 text-md text-center text-gray-400">
+            Our designers have meticulously curated each piece, drawing
+            inspiration from timeless classics while infusing a modern edge.
+          </h2>
 
-          <ul className="mt-16 grid gap-1 max-w-screen-xl mx-auto sm:grid-cols-2 lg:grid-cols-4">
+          <ul className="mt-10 grid gap-1 mb-5 max-w-screen-xl mx-auto sm:grid-cols-2 lg:grid-cols-4">
             <li>
               <p href="#" className="group block overflow-hidden">
                 <img
                   src="https://img.freepik.com/premium-photo/man-stands-street-wearing-green-jacket-black-cap_943281-23344.jpg?size=626&ext=jpg&ga=GA1.2.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                  className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -62,7 +66,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/angel-ai-generated_23-2150690347.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                  className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -87,7 +91,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/cyberpunk-warrior-urban-scenery_23-2150712356.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                  className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -112,7 +116,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/premium-photo/fashion-model-wearing-jacket_519183-9880.jpg?size=626&ext=jpg&ga=GA1.2.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                  className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -137,7 +141,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/cyberpunk-man-warrior-with-dog_23-2150712452.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                  className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -162,7 +166,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/close-up-man-portrait-new-york-city_23-2150868124.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                 className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -187,7 +191,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/close-up-man-new-york-streets_23-2150868150.jpg?size=626&ext=jpg&ga=GA1.2.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                 className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -212,7 +216,7 @@ const Collections = ({ carts }) => {
                 <img
                   src="https://img.freepik.com/free-photo/portrait-person-during-daily-life-new-york-city_23-2150820002.jpg?size=626&ext=jpg&ga=GA1.1.1850613524.1706461821&semt=ais_ai_generated"
                   alt=""
-                  className="h-[350px] mx-auto object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
+                 className="h-[350px] mx-auto mt-5 object-cover transition duration-500 group-hover:scale-105 sm:w-full sm:h-[450px]"
                 />
 
                 <div className="relative bg-white pt-3">
